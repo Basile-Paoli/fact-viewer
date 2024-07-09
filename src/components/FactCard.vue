1d52dda2f245be8e7f6fb8e3bc984cd69d2f1c09
Basile Paoli <basile.paoli@gmail.com>
2024-07-09 18:33:55 +0200

    implement data fetching

--- src/components/FactCard.vue
+++ src/components/FactCard.vue
@@ -1,21 +1,25 @@
 <script setup lang="ts">
 import {ref} from "vue";
 
-// const {label, getFact} = defineProps<{
-//     label: string,
-//     getFact: () => Promise<string>
-// }>()
-//
-// const fact = ref("")
+const {label, getFact} = defineProps<{
+    label: string,
+    getFact: () => Promise<string>
+}>()
+
+const fact = ref("")
+
+const getNewFact = async () => {
+    fact.value = await getFact()
+}
 </script>
 
 <template>
     <div class="rounded-2xl shadow-xl border border-black/25 bg-white h-[300px] w-[400px] p-8 m-4">
         <div class="flex justify-between">
-            <div>A cool <span class="font-bold underline">Math</span> Fact</div>
-            <button ><img class="bg-red-500 h-8" src="../assets/arrow-down.svg"/> </button>
+            <div>A cool <span class="font-bold underline">{{ label}}</span> Fact</div>
+            <button @click="getNewFact" ><img class="bg-red-500 h-8" src="../assets/arrow-down.svg"/> </button>
         </div>
-        <div class="font-bold mx-2 mt-4">1 + 1 = 2</div>
+        <div class="font-bold mx-2 mt-4">{{fact}}</div>
     </div>
 </template>
 
