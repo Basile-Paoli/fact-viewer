da56a31b8d7585b43121a73f21641da59781efe7
Basile Paoli <basile.paoli@gmail.com>
2024-07-09 18:10:12 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex justify-evenly ">
+        <div>Test</div>
+        <div>test</div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
