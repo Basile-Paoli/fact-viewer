5c7926b6e652bce7ffa481a52790fb4e45976160
Basile Paoli <basile.paoli@gmail.com>
2024-07-09 19:17:37 +0200

    fix button icon

--- src/components/FactCard.vue
+++ src/components/FactCard.vue
@@ -24,12 +24,13 @@ const getNewFact = async () => {
     <div class="rounded-2xl shadow-xl border border-black/25 bg-white h-[300px] w-[400px] p-8 my-10">
         <div class="flex justify-between">
             <div>A cool <span class="font-bold underline">{{ type }}</span> Fact</div>
-            <button @click="getNewFact"><img class="bg-red-500 h-8" src="../assets/arrow-down.svg"/></button>
+            <button @click="getNewFact"><img class="rounded-full bg-red-500 h-8" src="../assets/arrow-down.svg"/></button>
         </div>
         <div class="font-bold mx-2 mt-4">{{ fact }}</div>
     </div>
 </template>
 
 <style scoped>
+
 
 </style>
